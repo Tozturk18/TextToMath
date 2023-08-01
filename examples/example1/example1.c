5e3f3011f7d12edf6ccb1ae38caa79e4553a315f
tozturk18 <tunaozturk2001@hotmail.com>
2023-08-01 08:32:46 +0300

    adding more example files

---
+++ examples/example1/example1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../../textMath.h"
+
+int main() {
+    printf("\nWelcome! Thank you for trying out Text-To-Math program.\nPlease enter a mathematical expression below!\nCurrently the character limit is 225 character...\n\n");
+    char expression[225];
+    fgets(expression, sizeof(expression), stdin);
+    
+    float solution = textCalc(expression);
+    printf("Solution: %.2f\n", solution);
+
+    return 0;
+}
--- examples/example2/example2.c
+++ examples/example2/example2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../textMath.h"
+#include "../../textMath.h"
 
 char* anan(char* str) {
 
